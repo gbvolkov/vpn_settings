5aaaa2e74ea744b40b6ac4e72954cdf57b9bea43
gbvolkov <gbvolkov@gmail.com>
2026-04-04 11:13:42 +0300

    new update

--- tgupd.cmd
+++ tgupd.cmd
@@ -1,4 +1,5 @@
-SET TELEGRAM_API_ID=21555669
-SET TELEGRAM_API_HASH=e5374b740570ba97e3ff85fc6a6d8a4f
+@echo off
+set "TELEGRAM_API_ID=21555669"
+set "TELEGRAM_API_HASH=e5374b740570ba97e3ff85fc6a6d8a4f"
 
-python3 telegram_update.py --unblock unblock.txt --session-file .telegram_update.session
+py -3 telegram_update.py --unblock unblock.txt --session-file .telegram_update.session
